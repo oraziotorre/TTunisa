b4dff78dc0063a352ceb3a906871b536c16ee69b
Oras <oraziotorre03@gmail.com>
2024-07-04 16:45:08 +0200

    HomePageImages

--- src/main/webapp/js/homepage.js
+++ src/main/webapp/js/homepage.js
@@ -1,30 +1,20 @@
+let inTransition = false;
+const immagini = ['images/homeimg1.jpg', 'images/homeimg2.jpg'];
+let indiceImmagine = 0;
 
-    let inTransition = false; // Variabile per gestire la transizione
-
-    function cambiaImmagine() {
-    if (inTransition) return; // Evita di eseguire se è già in transizione
+function cambiaImmagine() {
+    if (inTransition) return;
     inTransition = true;
 
     let immagine = document.getElementById('immagine_grande');
-    immagine.style.opacity = 0; // Imposta opacità a 0 per farlo scomparire
+    immagine.style.opacity = 0;
 
-    // Cambia immagine dopo la durata della transizione (500ms)
     setTimeout(() => {
-    let immagineAttuale = immagine.src;
-
-    if (immagineAttuale.endsWith('home1.jpg')) {
-    immagine.src = 'images\\homeimg1.png';
-} else {
-    immagine.src = 'images\\home1.jpg';
+        indiceImmagine = (indiceImmagine + 1) % immagini.length;
+        immagine.src = immagini[indiceImmagine];
+        immagine.style.opacity = 1;
+        inTransition = false;
+    }, 500);
 }
 
-    immagine.style.opacity = 1; // Riporta opacità a 1 per far apparire la nuova immagine
-    document.querySelector("ul").style.zIndex='1000';
-    inTransition = false; // Transizione completata
-
-}, 500);
-}
-
-    /* l'intervallo cambia immagine automaticamente ogni 3 secondi */
-    setInterval(cambiaImmagine, 3000);
-
+setInterval(cambiaImmagine, 3000);
