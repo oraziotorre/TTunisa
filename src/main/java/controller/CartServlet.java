4c288341120354e972dea0a4332cdbda5d644da5
Oras <oraziotorre03@gmail.com>
2024-07-10 16:58:31 +0200

    bug cart

--- src/main/java/controller/CartServlet.java
+++ src/main/java/controller/CartServlet.java
@@ -7,7 +7,6 @@ import jakarta.servlet.http.*;
 import model.*;
 
 import java.io.IOException;
-import java.util.GregorianCalendar;
 
 @WebServlet("/cart")
 public class CartServlet extends HttpServlet {
@@ -20,44 +19,45 @@ public class CartServlet extends HttpServlet {
         Carrello cart = (Carrello) request.getSession().getAttribute("carrello");
         //cart.validazioneProdotti();   //Funzione per capire se i prodotti presenti nel carrello sono presenti nel db
 
-        if (action == null) {
+        if (action == null || action.isEmpty()) {
             RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/results/Carrello.jsp");
             dispatcher.forward(request, response);
-        }
-        if (action.equals("checkout")) {
+        }else {
+            if (action.equals("checkout")) {
 
-            HttpSession session = request.getSession();
-            Utente utenteLoggato = (Utente) session.getAttribute("Utente");
-            if (cart.checkout(utenteLoggato)) {
-                session.setAttribute("carrello", new Carrello());
-                response.sendRedirect("order_history");
-            } else {
-                ////ERRORE QUANTITA NON DISPONIBILE
-                response.sendRedirect("cart");
+                HttpSession session = request.getSession();
+                Utente utenteLoggato = (Utente) session.getAttribute("Utente");
+                if (cart.checkout(utenteLoggato)) {
+                    session.setAttribute("carrello", new Carrello());
+                    response.sendRedirect("order_history");
+                } else {
+                    ////ERRORE QUANTITA NON DISPONIBILE
+                    response.sendRedirect("cart");
+                }
             }
-        }
 
-        if (action.equals("additem")) {
-            //Dall'ID del prodotto prendo il prodotto e lo aggiungo al carrello
-            int ID = Integer.parseInt(request.getParameter("ID"));
-            Prodotto prodotto = ProdottoDAO.findProduct(ID);
-            prodotto.setQuantita(Integer.parseInt(request.getParameter("quantita")));
-            cart.addProdotto(prodotto);
-            response.sendRedirect(request.getContextPath() + "/cart");
-        }
+            if (action.equals("additem")) {
+                //Dall'ID del prodotto prendo il prodotto e lo aggiungo al carrello
+                int ID = Integer.parseInt(request.getParameter("ID"));
+                Prodotto prodotto = ProdottoDAO.findProduct(ID);
+                prodotto.setQuantita(Integer.parseInt(request.getParameter("quantita")));
+                cart.addProdotto(prodotto);
+                response.sendRedirect(request.getContextPath() + "/cart");
+            }
 
-        if (action.equals("removeitem")) {
-            int ID = Integer.parseInt(request.getParameter("ID"));
-            cart.removeProdotto(ID);
-            response.sendRedirect(request.getContextPath() + "/cart");
-        }
+            if (action.equals("removeitem")) {
+                int ID = Integer.parseInt(request.getParameter("ID"));
+                cart.removeProdotto(ID);
+                response.sendRedirect(request.getContextPath() + "/cart");
+            }
 
-        if (action.equals("updateitem")) {
+            if (action.equals("updateitem")) {
 
-            //GESTISCE LA RICHIESTA AJAX DA CARRELLO.jsp
-            int ID = Integer.parseInt(request.getParameter("ID"));
-            int quantita = Integer.parseInt(request.getParameter("quantita"));
-            cart.setNumOrdered(ID, quantita);
+                //GESTISCE LA RICHIESTA AJAX DA CARRELLO.jsp
+                int ID = Integer.parseInt(request.getParameter("ID"));
+                int quantita = Integer.parseInt(request.getParameter("quantita"));
+                cart.setNumOrdered(ID, quantita);
+            }
         }
     }
 
